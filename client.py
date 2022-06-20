f247e91c69b2e7956ff668de1e67b85c2c7fd709
ChngJeremy <chng_jeremy@outlook.com>
2022-06-20 11:45:23 +0800

    refector stubs and menu

--- client.py
+++ client.py
@@ -7,65 +7,158 @@ from mimetypes import init
 import grpc
 
 import Tracetogether_pb2, Tracetogether_pb2_grpc
-class client:
-
-def SetUsersData():
-    # open a gRPC channel
-    channel = grpc.insecure_channel('localhost:50051')
-    # create a stub (client)
-    stub = Tracetogether_pb2_grpc.UserInfoStub(channel)
-    #input data
-    user_id  = int(input('Enter User Id : '))
-    username = input('Enter Username : ')
-    password = input('Enter Password : ')
-    # create a valid request message
-    data = Tracetogether_pb2.UserRequest(user_id=user_id,name=username,password=password)
-    # make the call
-    response = stub.Username(data)
-    # response
-    print(response.name)
 
 def menu():
-    print("[1] Individual Checkin/ Checkout")
-    print("[2] Group Checkin/ Checkout")
-    print("[3] Add Family Members into Group")
-    print("[4] View History")
-    print("[5] Declare Covid Clusters")
-    print("[0] Exit Program")
+    print("[1] Individual Check In")
+    print("[2] Individual Check Out")
+    print("[3] Group Check In")
+    print("[4] Group Check Out")
+    print("[5] Get History")
+    print("[6] Check Cases")
+    print("[7] Flag Cases")
+    print("[0] Exit")
+
+'''
+GRPC Client
+'''
+
+'''
+Check-in Functionality (Individual)
+'''
+def check_in(stub,name,nric,location,check_in):
+    response=stub.check_in(Tracetogether_pb2.Check_inRequest(name=name,nric=nric,location=location,check_in=check_in))
+    print(response.message)
+
+
+'''
+Check-out Functionality (Individual)
+'''
+def check_out(stub,name,nric,location,check_out):
+    response=stub.check_out(Tracetogether_pb2.Check_outRequest(name=name,nric=nric,location=location,check_out=check_out))
+    print(response.message)
+
+'''
+Check-in Functionality (Group)
+'''
+def check_in_grp(stub, nameList: list, nricList: list, location, check_in):
+    if type(nameList) != list or type(nricList) != list:
+        print("Failure")
+        print("Input must be input in a list E.g. [\"Ivan\",\"Jeremy\"] or [\"Sxxxxxx\",\"Sxxxxxx\"]")
+        return
+
+    if len(nameList) != len(nricList):
+        print("Failure")
+        print("Number of names and nrics must be equal")
+        return
+
+    response=stub.check_in_grp(Tracetogether_pb2.Check_in_grpRequest
+    (name=nameList,nric=nricList,location=location,check_in=check_in))
+    print(response.message)
+
+
+'''
+Check-out Functionality (Group)
+'''
+def check_out_grp(stub, nameList: list, nricList: list, location, check_out):
+    if type(nameList) != list or type(nricList) != list:
+        print("Failure")
+        print("Input must be input in a list E.g. [\"Ivan\",\"Jeremy\"] or [\"Sxxxxxx\",\"Sxxxxxx\"]")
+        return
+
+    if len(nameList) != len(nricList):
+        print("Failure")
+        print("Number of names and nrics in the list must be equal")
+        return
+
+    response=stub.check_out_grp(Tracetogether_pb2.Check_out_grpRequest
+    (name=nameList,nric=nricList,location=location,check_out=check_out))
+    print(response.message)
+
+'''
+Get History Functionality
+'''
+def get_history(stub,name,nric):
+    response=stub.get_history(Tracetogether_pb2.Get_historyRequest(name=name,nric=nric))
+    print(response.message)
+
+    for location in response.location:
+        print(location)
+
+def check_cases(stub,name,nric):
+    response=stub.check_cases(Tracetogether_pb2.Check_casesRequest(name=name,nric=nric))
+    print(response.message)
+
+def flag_cases(stub,name,nric):
+    response=stub.flag_cases(Tracetogether_pb2.Flag_casesRequest(name=name,nric=nric))
+    print(response.message)
+
+
+def get_time():
+    now=datetime.now()
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 if __name__ == '__main__':
     logging.basicConfig()
-    SetUsersData()
-    menu()
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = Tracetogether_pb2_grpc.TracetogetherStub(channel)
+        '''
+    [1] Individual Check In
+    [2] Individual Check Out
+    [3] Group Check In
+    [4] Group Check Out
+    [5] Get History
+    [6] Check Cases
+    [7] Flag Cases
+    [0] Exit
+        '''
+        while True:
+            menu()
+            choice = int(input())
+            if choice == 1:
+                name = input("Enter name: ")
+                nric = input("Enter nric: ")
+                location = input("Enter location: ")
+                check_in = get_time()
+                check_in(stub,name,nric,location,check_in)
 
-    while True:
-        try:
-            option = int(input("Enter an option: "))
-            if option == "1":
-                check_in = input("Enter your location:")
-                print('You have Checked in at ' + check_in + ' at ' + str(datetime.now()))
-                
-                
+            elif choice == 2:
+                name = input("Enter name: ")
+                nric = input("Enter nric: ")
+                location = input("Enter location: ")
+                check_out = get_time()
+                check_out(stub,name,nric,location,check_out)
 
-            elif option == "2":
-                print("Group Checkin/ Checkout Called\n")
-                print('You have Checked out at'+ check_in + ' at ' + str(datetime.now()))
-
-            elif option == "3":
-                print("Add Family Members into Group Called\n")
-
-            elif option == "4":
-                print("View History Called\n")
-
-            elif option == "5":
-                print("Declare Covid Clusters Called\n")
-
-            elif option == "0":
-                print("End Program\n")
-                exit()
+            elif choice == 3:
+                nameList = input("Enter name: ")
+                nricList = input("Enter nric: ")
+                location = input("Enter location: ")
+                check_in = get_time()
+                check_in_grp(stub,nameList,nricList,location,check_in)
             
+            elif choice == 4:
+                nameList = input("Enter name: ")
+                nricList = input("Enter nric: ")
+                location = input("Enter location: ")
+                check_out = get_time()
+                check_out_grp(stub,nameList,nricList,location,check_out)
+            
+            elif choice == 5:
+                name = input("Enter name: ")
+                nric = input("Enter nric: ")
+                get_history(stub,name,nric)
+
+            elif choice == 6:
+                name = input("Enter name: ")
+                nric = input("Enter nric: ")
+                check_cases(stub,name,nric)
+
+            elif choice == 7:
+                name = input("Enter name: ")
+                nric = input("Enter nric: ")
+                flag_cases(stub,name,nric)
+
+            elif choice == 0:
+                exit()
             else:
-                raise ValueError
-        
-        except ValueError:
-            print("Enter an valid option!\n")
+                print("Invalid input")
