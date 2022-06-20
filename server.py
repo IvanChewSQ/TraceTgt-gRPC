a0f4010eea0e3f96c9a6c8f3ebbde1f15d160dd0
ChngJeremy <chng_jeremy@outlook.com>
2022-06-20 12:43:39 +0800

    configured protos and added test data

--- server.py
+++ server.py
@@ -20,60 +20,67 @@ import logging
 import time
 import json
 import grpc
-import Tracetogether_pb2, Tracetogether_pb2_grpc
+import test_db
+import Tracetogether_pb2 as Tracetogether_pb2, Tracetogether_pb2_grpc as Tracetogether_pb2_grpc
 
-class UserInfoService(Tracetogether_pb2_grpc.UserInfoServicer):
-    def Username(self, request, context):
-        response = Tracetogether_pb2.UserRequest()
-        response.name = users_method.users_info(request.user_id,request.name,request.password)
-        return response
-
-
-
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-Tracetogether_pb2_grpc.add_UserInfoServicer_to_server(
-        UserInfoService(), server)
-print('Starting server. Listening on port 50051.')
-server.add_insecure_port('[::]:50051')
-server.start()
-try:
-    while True:
-        time.sleep(86400)
-except KeyboardInterrupt:
-    server.stop(0)
-class Checkin(Tracetogether_pb2_grpc.CheckinServicer):
-
-  def details(self, request, context):
-    data = {
-        "Name": request.name,
-        "NRIC": request.nric,
-        "Location": request.location,
-        "Status": request.status,
-        "Date": date.today().strftime("%B %d, %Y"),
-        "Time": datetime.now().strftime("%H:%M:%S")
-    }
-
-    try:
-      with open('data.json', 'w') as json_file:
-        json.dump(data, json_file)
-        return Tracetogether_pb2.details(message='%s, Completed!' % request.status)
-    except: 
-      return Tracetogether_pb2.details(message='%s, Unsuccessful! Please try again...' % request.status)
+class SafeEntry(Tracetogether_pb2_grpc.TracetogetherServicer):
     
+    def __init__(self):
+        self.db = test_db.Database()
+    
+    def Check_In(self, request, context):
+        print(request.nric, request.location)
+        self.db.addData(request.name, request.nric, request.location, request.checkin)
+        return Tracetogether_pb2.Check_In_Reply(message="Check-In Successful")
+
+    def Check_Out(self, request, context):
+        print(request.nric, request.checkout)
+        self.db.updateData(request.nric, request.checkout)
+        return Tracetogether_pb2.Check_Out_Reply(message="Check-Out Successful")
+
+    def Check_In_Group(self, request, context):
+        index = 0
+        for ic in request.nric:
+            self.db.addData(request.name[index], ic, request.location, request.checkin)
+            print(request.name[index], ic)
+            index += 1
+        return Tracetogether_pb2.Grp_Check_In_Reply(message="Group Check-In Successful")
+
+    def Check_Out_Group(self, request, context):
+        for ic in request.nric:
+            self.db.updateData(ic, request.checkout)
+            print(ic)
+        return Tracetogether_pb2.Grp_Check_Out_Reply(message=" GroupCheck Out Successful")
+
+    def Location_history(self, request, context):
+        locations = self.db.getVisited(request.nric)
+        return Tracetogether_pb2.History_Reply(locations=locations)
+
+
+
+    def Check_cases(self, request, context):
+        return Tracetogether_pb2.Location_Reply(locationList=self.db.getCases(request.nric, self.db.getLocation()))
+
+    def Flag_cases(self, request, context):
+        self.db.addCase(request.location, request.datetime)
+        return Tracetogether_pb2.Flag_Reply(message="Flagged Case Successful")
+
+    def Flag_location(self, request, context):
+        self.db.addLocation(request.location, request.datetime)
+        return Tracetogether_pb2.Flag_Reply(message="Flagged Location Successful")
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    (Checkin(), server)
     listen_addr = '[::]:50051'
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    Tracetogether_pb2_grpc.add_TracetogetherServicer_to_server(SafeEntry(), server)
+
     server.add_insecure_port(listen_addr)
     logging.info('TraceTogether server starting on %s', listen_addr)
     print('TraceTogether server starting on %s', listen_addr)
 
-    Tracetogether_pb2_grpc.add_CheckinServicer_to_server(Checkin(),server)
-    
-    server.start()
-    server.wait_for_termination()
 
+    server.wait_for_termination()
 
 if __name__ == '__main__':
     logging.basicConfig()
